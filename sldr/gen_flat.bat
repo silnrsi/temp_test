730a4bb40f7e50facb5fdb0e33c7f0f8cc308985
dallas\correll <dallas\correll@sil.org>
2015-02-18 09:08:31 -0600

    Fixed comments in .bat files

--- sldr/gen_flat.bat
+++ sldr/gen_flat.bat
@@ -1,5 +1,5 @@
 md flat
-rem -a creates a separate subdirectory for each letter of the alphabet
-rem -s ????
+rem -a = creates a separate subdirectory for each letter of the alphabet
+rem -s = turn off multiprocessing
 python ..\..\..\sites\s\scripts\ldml_utils\ldmlflatten.py -o .\flat -a -i ..\..\..\..\ss_import\data\original\SLDR-Git\sldr\sldr -s
 
--- sldr/gen_unflat.bat
+++ sldr/gen_unflat.bat
@@ -1,5 +1,6 @@
-md flat
-rem -a creates a separate subdirectory for each letter of the alphabet
-rem -s ????
+md unflat
+rem -a = creates a separate subdirectory for each letter of the alphabet
+rem -s = turn off multiprocessing
+rem -r = create unflat (minimal) files
 python ..\..\..\sites\s\scripts\ldml_utils\ldmlflatten.py -o .\unflat -r -a -i ..\..\..\..\ss_import\data\original\SLDR-Git\sldr\sldr -s
 
