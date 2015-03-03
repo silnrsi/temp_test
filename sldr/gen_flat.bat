2fc0d94ff4918f052b4948f5d46d1b5f28cdcab7
dallas\correll <dallas\correll@sil.org>
2015-03-03 08:33:55 -0600

    Regenerate flat/unflat files to match 26Feb changes to CLDR handling (include more regions); update batch files.

--- sldr/gen_flat.bat
+++ sldr/gen_flat.bat
@@ -1,6 +1,8 @@
 rem Assumes the ldmlflatten file is updated from Git:
 copy C:\xampp\htdocs\ss_import\data\original\SLDR-Git\sldr\python\scripts\ldmlflatten ..\..\..\sites\s\scripts\ldml_utils\ldmlflatten.py
 
+copy C:\xampp\htdocs\ss_import\data\original\SLDR-Git\sldr\python\lib\sldr\*.xml ..\..\..\sites\s\scripts\ldml_utils\sldr\*.xml
+
 md flat
 
 rem -a = creates a separate subdirectory for each letter of the alphabet
